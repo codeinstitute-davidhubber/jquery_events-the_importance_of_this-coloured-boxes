3476e9a5cf696757c3b9c9aec7e09d7f8c639efb
David Hubber <david.hubber@icloud.com>
2020-07-09 15:22:23 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,9 +1,5 @@
 $(document).ready(function() {
     $(".box").on("click", function() {
-    	/**
-    	 * When we click on an element that has
-    	 * a 'box' class, this event will be fired.
-    	 */
     	var classNames = $(this).attr("class").split(" ");
         $("." + classNames[1]).css("background-color", "#cf5857");
     });
